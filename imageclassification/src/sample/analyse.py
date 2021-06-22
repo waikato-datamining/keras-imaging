e6f1266aa6594894340d4a2f2a3e7f1ebd534953
csterling <coreytsterling@gmail.com>
2021-06-23 09:30:44 +1200

    Fixed analyse script.

--- imageclassification/src/sample/analyse.py
+++ imageclassification/src/sample/analyse.py
@@ -1,24 +1,21 @@
 import os
 import sys
-from collections import OrderedDict
 
 from sklearn.metrics import top_k_accuracy_score, hinge_loss
 
-from sample import split_arg, load_dataset, load_predictions, per_label
+from sample import split_arg, load_dataset, load_predictions, label_indices
 
 SOURCE_PATH, SOURCE_DATASET, SOURCE_EXT = split_arg(sys.argv[1])
 
 source_dataset_without_data = load_dataset(os.path.join(SOURCE_PATH, SOURCE_DATASET + SOURCE_EXT))
 
-pl = per_label(source_dataset_without_data)
+label_indices = label_indices(source_dataset_without_data)
 
-for label, ds in pl.items():
-    print(f"{label}\t{len(ds)}")
+numeric_labels = list(range(len(label_indices)))
 
-label_indices = OrderedDict()
-for label in source_dataset_without_data.values():
-    if label not in label_indices:
-        label_indices[label] = len(label_indices)
+holdout_dataset = load_dataset("holdout.txt")
+
+y_true = [label_indices[label] for label in holdout_dataset.values()]
 
 iteration = 0
 while True:
@@ -29,22 +26,21 @@ while True:
 
     predictions = load_predictions(filename)
 
-    y_true = [label_indices[label] for label, _ in predictions.values()]
-    y_score = [probs for _, probs in predictions.values()]
+    y_score = list(predictions.values())
 
     top_1 = top_k_accuracy_score(
         y_true,
         y_score,
         k=1,
-        labels=list(range(len(label_indices)))
+        labels=numeric_labels
     )
 
     hinge = hinge_loss(
         y_true,
         y_score,
-        labels=list(range(len(label_indices)))
+        labels=numeric_labels
     )
 
-    print(hinge)
+    print(f"{hinge},{top_1}")
 
     iteration += 1
