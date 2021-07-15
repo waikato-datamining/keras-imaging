8d53c965ed03d4abd5e654ebd11fcd5648530e3f
csterling <coreytsterling@gmail.com>
2021-07-16 09:40:18 +1200

    Exported change_filename.

--- imageclassification/src/sample/__init__.py
+++ imageclassification/src/sample/__init__.py
@@ -7,5 +7,5 @@ from ._kernel import Kernel, CachedKernel, RBFKernel, RBFKernel2
 from ._load import load_dataset, load_predictions, load_rois_predictions
 from ._math import factorial, number_of_subsets, subset_number_to_subset, subset_to_subset_number
 from ._types import Dataset, Split, Predictions
-from ._util import split_arg, merge, first, per_label, label_indices, predictions_file_header, top_n, shuffle_dataset, change_path, rm_dir, run_command, coerce_incorrect
+from ._util import split_arg, merge, first, per_label, label_indices, predictions_file_header, top_n, shuffle_dataset, change_path, rm_dir, run_command, coerce_incorrect, change_filename
 from ._write import write_dataset, write_predictions
